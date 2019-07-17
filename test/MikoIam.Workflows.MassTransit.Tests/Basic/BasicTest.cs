1e2d29c3ad07cb5570d927e2ddfe67de9c99fb4e
Michał Kozłowski <40733674+mikoiam@users.noreply.github.com>
2019-07-17 08:28:26 +0200

    add test for workflow events reporting

--- test/MikoIam.Workflows.MassTransit.Tests/Basic/BasicTest.cs
+++ test/MikoIam.Workflows.MassTransit.Tests/Basic/BasicTest.cs
@@ -13,7 +13,7 @@ namespace MikoIam.Workflows.MassTransit.Tests.Basic
             // Arrange
             var aCalled = false;
             var bCalled = false;
-            var workflow = new BasicWorkflow(() => { aCalled = true;}, () => { bCalled = true;});
+            var workflow = new BasicWorkflow(() => { aCalled = true; }, () => { bCalled = true; });
             var observer = new SingleWorkflowObserver(workflow);
 
             var bus = Bus.Factory.CreateUsingInMemory(sbc =>
@@ -26,13 +26,37 @@ namespace MikoIam.Workflows.MassTransit.Tests.Basic
 
             await bus.Publish(new StartWorkflowMessage());
             observer.WorkflowFinishedHandle.Wait(TimeSpan.FromSeconds(5));
-            
+
             bus.Stop();
 
             // Assert
             Assert.Equal("AB", observer.TaskOrder);
             Assert.True(aCalled);
             Assert.True(bCalled);
+        }
+
+        [Fact]
+        public async Task ShouldReportWorkflowEventsInCorrectOrder()
+        {
+            // Arrange
+            var workflow = new BasicWorkflow(() => { }, () => { });
+            var observer = new SingleWorkflowObserver(workflow);
+
+            var bus = Bus.Factory.CreateUsingInMemory(sbc =>
+            {
+                sbc.ReceiveEndpoint("basic_workflow", ep => { ep.Workflow(workflow); });
+            });
+
+            // Act
+            bus.Start();
+
+            await bus.Publish(new StartWorkflowMessage());
+            observer.WorkflowFinishedHandle.Wait(TimeSpan.FromSeconds(5));
+
+            bus.Stop();
+
+            // Assert
+            Assert.Equal("@WF@A$A@B$B$WF", observer.EventOrder);
         }
     }
 }
--- test/MikoIam.Workflows.MassTransit.Tests/Basic/SingleWorkflowObserver.cs
+++ test/MikoIam.Workflows.MassTransit.Tests/Basic/SingleWorkflowObserver.cs
@@ -6,17 +6,27 @@ namespace MikoIam.Workflows.MassTransit.Tests.Basic
 {
     public class SingleWorkflowObserver
     {
-        private readonly StringBuilder _taskOrder;
+        private readonly StringBuilder _taskOrder = new StringBuilder();
+        private readonly StringBuilder _eventOrder = new StringBuilder();
 
-        public ManualResetEventSlim WorkflowFinishedHandle { get; }
+        public ManualResetEventSlim WorkflowFinishedHandle { get; } = new ManualResetEventSlim();
         public string TaskOrder => _taskOrder.ToString();
+        public string EventOrder => _eventOrder.ToString();
 
         public SingleWorkflowObserver(Workflow workflow)
         {
-            _taskOrder = new StringBuilder();
-            WorkflowFinishedHandle = new ManualResetEventSlim();
-            workflow.WorkflowFinished += (sender, args) => WorkflowFinishedHandle.Set();
-            workflow.TaskFinished += (sender, args) => _taskOrder.Append(args.TaskId);
+            workflow.WorkflowStarted += (sender, args) => { _eventOrder.Append("@WF"); };
+            workflow.WorkflowFinished += (sender, args) =>
+            {
+                _eventOrder.Append("$WF");
+                WorkflowFinishedHandle.Set();
+            };
+            workflow.TaskStarted += (sender, args) => { _eventOrder.Append($"@{args.TaskId}"); };
+            workflow.TaskFinished += (sender, args) =>
+            {
+                _eventOrder.Append($"${args.TaskId}");
+                _taskOrder.Append(args.TaskId);
+            };
         }
     }
 }
